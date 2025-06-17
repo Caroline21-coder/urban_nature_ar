464229f062282f26c77342191fa6333f110e93c9
Caroline21-coder <caroline.cesselin@ucdconnect.ie>
2025-06-17 11:39:57 +0200

    Updated step3.tsx, removed index.tsx, added AR.tsx and .env

---
+++ app/AR.tsx
@@ -0,0 +1,66 @@
+import { View, Text, ImageBackground, Pressable, StyleSheet, Dimensions } from "react-native";
+import { useRouter } from "expo-router";
+
+const { width, height } = Dimensions.get("window");
+
+export default function Welcome() {
+  const router = useRouter();
+
+  return (
+    <ImageBackground
+      source={require("../assets/images/dandelion.jpg")}
+      style={styles.background}
+      resizeMode="cover"
+    >
+      <View style={styles.overlay}>
+        <Text style={styles.title}>EXPLORE URBAN NATURE</Text>
+        <Pressable style={styles.button} onPress={() => router.replace("/explanation/step1")}>
+          <Text style={styles.buttonText}>Start now</Text>
+        </Pressable>
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    width: width,
+    height: height,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  overlay: {
+    flex: 1,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgba(14,61,59,0.25)", // Optional: dark overlay for readability
+    padding: 24,
+  },
+  title: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "400",
+    letterSpacing: 1,
+    marginTop: 1,
+    marginBottom: 10,
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: "#f8f8e7",
+    borderRadius: 24,
+    paddingVertical: 10,
+    paddingHorizontal: 48,
+    alignItems: "center",
+    position: "absolute",
+    bottom: 60,
+    alignSelf: "center",
+    width: width * 0.8,
+  },
+  buttonText: {
+    color: "#194038",
+    fontSize: 18,
+    fontWeight: "500",
+  },
+});
